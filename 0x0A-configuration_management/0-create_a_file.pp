7d3427b0301f169925da839585f4ebfb4dfd54c9
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-03-23 12:43:42 +0000

    Task 0: create file with puppet CM

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+file { '/tmp/holberton'
+  path  => '/tmp/holberton',
+  ensure  => 'present',
+  content  => 'I love Puppet',
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode  => '0744',
+}
