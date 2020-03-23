4a741f137660466e9141bfc59a321a0eaa5e8355
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-03-23 13:08:41 +0000

    Puppet-lint verified

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,8 @@
-file { '/tmp/holberton'
-  path  => '/tmp/holberton',
+file { '/tmp/holberton':
   ensure  => 'present',
-  content  => 'I love Puppet',
-  owner  => 'www-data',
-  group  => 'www-data',
-  mode  => '0744',
+  path    => '/tmp/holberton',
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
 }
