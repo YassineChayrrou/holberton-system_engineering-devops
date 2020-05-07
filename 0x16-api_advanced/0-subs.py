77134477a0da52567896d31ee28db8115c16a4a5
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-07 23:07:21 +0000

    test

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,9 +7,9 @@ import json
 def number_of_subscribers(subreddit):
     """returns the number of subscribers for a given subreddit
     """
-    headers = {'user-agent': 'potato'}
+    headers = {'user-agent': 'auserrequest'}
     r = requests.get("http://www.reddit.com/r/{}/about.json".format(subreddit))
-    if r.status_code is 302 or r.status_code is 404:
+    if (r.status_code is 302) or (r.status_code is 404):
         return 0
     r = r.json()
     if 'error' in r:
@@ -17,4 +17,3 @@ def number_of_subscribers(subreddit):
     if 'subscribers' in r['data']:
         return r['data']['subscribers']
     return 0
-    
