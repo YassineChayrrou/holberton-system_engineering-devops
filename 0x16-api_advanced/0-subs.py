45d568c8a7acb02c751021456c0ee9832f4e59a1
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-07 23:02:00 +0000

    First task

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import requests
+import sys
+import json
+
+
+def number_of_subscribers(subreddit):
+    """returns the number of subscribers for a given subreddit
+    """
+    headers = {'user-agent': 'potato'}
+    r = requests.get("http://www.reddit.com/r/{}/about.json".format(subreddit))
+    if r.status_code is 302 or r.status_code is 404:
+        return 0
+    r = r.json()
+    if 'error' in r:
+        return 0
+    if 'subscribers' in r['data']:
+        return r['data']['subscribers']
+    return 0
+    
