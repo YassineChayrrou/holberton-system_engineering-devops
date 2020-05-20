2a3d6e9fb07edb71bc780523ea05fa9e627643ab
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-05-20 20:55:57 +0000

    Puppet script to fix line 137 in wp-settings.php causing 500 internal error

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # fix wordpress error 500 puppet automation
 exec { 'wp-settingsFix':
-  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php"
+  command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
   path    => [/bin]
 }
