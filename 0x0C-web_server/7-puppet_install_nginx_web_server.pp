e72133bb486f33d0faa692f61833c99c127aa846
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-07-04 01:40:14 +0100

    check puppet lint

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,33 +1,33 @@
 #nginx install and configuration as mentioned on task 3 using puppet
 
-exec {'update server':
+exec {'update-server':
   provider => shell,
   path     => '/usr/bin',
   command  => 'sudo apt-get -y update',
 }
 
-exec {'install nginx':
+exec {'install-nginx':
   provider => shell,
   path     => '/usr/bin',
   command  => 'sudo apt-get -y install nginx',
 }
 
-exec {'custom html':
+exec {'custom-html':
   provider => shell,
   command  => 'sudo echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
 }
 
-exec {'start nginx':
+exec {'start-nginx':
   provider => shell,
   command  => 'sudo service nginx start',
 }
 
-exec {'redirection setup':
+exec {'redirection-setup':
   provider => shell,
-  command  => 'sudo sed -n -i "p;19a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-enabled/default',
+  command  => "sudo sed -n -i 'p;19a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-enabled/default",
 }
 
-exec {'restart nginx':
+exec {'restart-nginx':
   provider => shell,
   command  => 'sudo service nginx restart',
 }
