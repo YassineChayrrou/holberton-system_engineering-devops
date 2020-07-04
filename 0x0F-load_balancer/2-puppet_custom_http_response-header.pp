5c646dab37033e4e03a71150e73bcf5d2839f5a1
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-07-04 23:46:51 +0100

    custom http header using  puppet lint

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -4,3 +4,8 @@ exec { 'Header_setup':
 command  => 'sudo sed -i "11i\	add_header X-Served-By $HOSTNAME;" /etc/nginx/nginx.conf',
 provider => 'shell',
 }
+
+exec { 'restart':
+  command  => 'service nginx restart',
+  provider => 'shell',
+}
