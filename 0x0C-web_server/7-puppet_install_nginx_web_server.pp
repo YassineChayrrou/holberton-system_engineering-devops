d504867b9af0553d298a5458d26f34ed2216b7ce
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-07-04 19:18:34 +0100

    puppet nginx automated install

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,34 @@
+#nginx install and configuration as mentioned on task 3 using puppet
+
+exec {'update-server':
+  provider => shell,
+  path     => '/usr/bin',
+  command  => 'sudo apt update -y',
+}
+
+exec {'install-nginx':
+  provider => shell,
+  path     => '/usr/bin',
+  command  => 'sudo apt install nginx -y',
+}
+
+exec {'custom-html':
+  provider => shell,
+  command  => 'echo "Holberton School" | sudo tee /var/www/html/index.nginx-debian.html',
+}
+
+
+exec {'redirection-setup':
+  provider => shell,
+  command  => "sudo sed -n -i 'p;19a rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;' /etc/nginx/sites-enabled/default",
+}
+
+exec {'start-nginx':
+  provider => shell,
+  command  => 'service nginx start',
+}
+
+exec {'start-nginx':
+  provider => shell,
+  command  => 'service nginx reload',
+}
