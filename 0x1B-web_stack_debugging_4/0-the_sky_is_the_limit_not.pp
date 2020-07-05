37d7ff44b812fb0a422a41d4ff08952bc722393d
YassineChayrrou <yassine.chayrrou@gmail.com>
2020-07-05 18:04:22 +0100

    fixed sed mistyped

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # customize nginx for better benchmark when testing server under requests pressure
 
 exec {'ab-fix':
-  command  => 'sed-i "s/worker_processes 4;/worker_processes 8;/g" /etc/nginx/nginx.conf && /etc/init.d/nginx restart',
+  command  => 'sed -i "s/worker_processes 4;/worker_processes 8;/g" /etc/nginx/nginx.conf && /etc/init.d/nginx restart',
   path     => '/bin/',
   provider => 'shell',
 }
